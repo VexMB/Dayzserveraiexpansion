d8d1e9a5d2a23ea9589d7ebbef7ce5ea560258d1
DESKTOP-6P7DMDG\grand <benappmain@gmail.com>
2025-12-20 08:49:33 +0000

    ai changes, base settings reverted

--- mpmissions/Expansion.chernarusplus/init.c
+++ mpmissions/Expansion.chernarusplus/init.c
@@ -1,94 +1,48 @@
 void main()
 {
-	//INIT ECONOMY--------------------------------------
+	// --- ECONOMY INITIALIZATION ---
 	Hive ce = CreateHive();
 	if ( ce )
 		ce.InitOffline();
 
-	//DATE RESET AFTER ECONOMY INIT-------------------------
+	// --- DATE AND TIME (DECEMBER 2025) ---
 	int year, month, day, hour, minute;
-	int reset_month = 9, reset_day = 20;
-	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+	GetGame().GetWorld().GetDate( year, month, day, hour, minute );
 
-	if ((month == reset_month) && (day < reset_day))
+	if ( year < 2025 )
 	{
-		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-	}
-	else
-	{
-		if ((month == reset_month + 1) && (day > reset_day))
-		{
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else
-		{
-			if ((month < reset_month) || (month > reset_month + 1))
-			{
-				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-			}
-		}
+		year = 2015;
+		month = 4;
+		day = 8;
+		hour = 12;
+		minute = 0;
+		GetGame().GetWorld().SetDate( year, month, day, hour, minute );
 	}
 }
 
 class CustomMission: MissionServer
 {
-	void SetRandomHealth(EntityAI itemEnt)
+	override void OnInit()
 	{
-		if ( itemEnt )
-		{
-			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
-			itemEnt.SetHealth01( "", "", rndHlt );
-		}
+		super.OnInit();
 	}
 
 	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
 	{
 		Entity playerEnt;
-		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
-		Class.CastTo( m_player, playerEnt );
+		playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");
+		Class.CastTo(m_player, playerEnt);
 
-		GetGame().SelectPlayer( identity, m_player );
+		// FIX: Only use two parameters (identity, playerObject)
+		GetGame().SelectPlayer(identity, m_player);
 
 		return m_player;
 	}
 
 	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
 	{
-		EntityAI itemClothing;
-		EntityAI itemEnt;
-		ItemBase itemBs;
-		float rand;
-
-		itemClothing = player.FindAttachmentBySlotName( "Body" );
-		if ( itemClothing )
-		{
-			SetRandomHealth( itemClothing );
-			
-			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			player.SetQuickBarEntityShortcut(itemEnt, 2);
-			
-			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
-			int rndIndex = Math.RandomInt( 0, 4 );
-			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-			SetRandomHealth( itemEnt );
-			player.SetQuickBarEntityShortcut(itemEnt, 1);
-
-			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
-			if ( rand < 0.35 )
-				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
-			else if ( rand > 0.65 )
-				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
-			else
-				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
-			player.SetQuickBarEntityShortcut(itemEnt, 3);
-			SetRandomHealth( itemEnt );
-		}
-		
-		itemClothing = player.FindAttachmentBySlotName( "Legs" );
-		if ( itemClothing )
-			SetRandomHealth( itemClothing );
-		
-		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+		player.RemoveAllItems();
+		// Note: Expansion handles loadouts in DayZExpansion/Settings/SpawnSettings.json
 	}
 };
 
